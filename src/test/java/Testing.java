10be9857c0bd0a783748185d65f1cd0ea13c2c46
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-12-05 21:12:05 -0500

    Test: Created test function for taking a list of integers and returning a map

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -20,5 +20,28 @@ class Testing {
        }//end of for loop
     
 	}//end of TestGenerateRandomList
+	
+	
+	@Test
+	public void testMapOccurrences() {
+		
+        List<Integer> inputList = List.of(1, 2, 2, 3, 3, 3);
+
+        Map<Integer, Integer> occurrences = IntegerOccurrencesMapper.mapOccurrences(inputList);
+
+       
+        assertEquals(3, occurrences.size(), "The map should have 3 unique keys");
+
+       
+        assertEquals(1, occurrences.get(1), "The count for 1 should be 1");
+        assertEquals(2, occurrences.get(2), "The count for 2 should be 2");
+        assertEquals(3, occurrences.get(3), "The count for 3 should be 3");
+
+        // Test with an empty list
+        Map<Integer, Integer> emptyOccurrences = IntegerOccurrencesMapper.mapOccurrences(Collections.emptyList());
+        
+        assertTrue(emptyOccurrences.isEmpty(), "The map should be empty for an empty input list");
+    
+	}//end of testMapOccurrences
 
 }//end of testing class
